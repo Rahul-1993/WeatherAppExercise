7a35077a61719289c241bd4102b369adc49bb226
Rahul Avale <rahulavale9@gmail.com>
2024-09-18 07:45:09 -0400

    Remove Redundant commented code

--- WeatherApp/Service/OpenWeatherService.swift
+++ WeatherApp/Service/OpenWeatherService.swift
@@ -17,31 +17,6 @@ class OpenWeatherService: WeatherService {
     private let apiKey = "d13f9c37068dc04df45dc55c2d5e17ff"
     private let units = "metric"
     
-//    func fetchWeather(for city: String) -> AnyPublisher<Weather, Error> {
-//        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=\(units)"
-//        guard let url = URL(string: urlString) else {
-//            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
-//        }
-//        return URLSession.shared.dataTaskPublisher(for: url)
-//            .map(\.data)
-//            .decode(type: Weather.self, decoder: JSONDecoder())
-//            .receive(on: DispatchQueue.main)
-//            .eraseToAnyPublisher()
-//    }
-//    
-//    func fetchWeatherByCoordinates(latitude: Double, longitude: Double) -> AnyPublisher<Weather, Error> {
-//        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
-//        guard let url = URL(string: urlString) else {
-//            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
-//        }
-//        
-//        return URLSession.shared.dataTaskPublisher(for: url)
-//            .map { $0.data }
-//            .decode(type: Weather.self, decoder: JSONDecoder())
-//            .receive(on: DispatchQueue.main)
-//            .eraseToAnyPublisher()
-//    }
-    
     // Generic function to fetch weather data
         private func fetchWeather(from urlString: String) -> AnyPublisher<Weather, Error> {
             guard let url = URL(string: urlString) else {
